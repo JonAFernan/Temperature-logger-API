42ebf126a6cb672335dce1c177c2f755323a37ba
Jon A.Fernadez <jafernandiaz@gmail.com>
2025-04-13 12:02:50 +0200

    database connection done

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import './env.js';
+import mysql from 'mysql2/promise';
+
+const connectDB = async () => {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    });
+
+    console.log('MySQL Database Connected Successfully!');
+    return connection;
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './config/env.js';
+import connectDB from './config/db.js';
 import httpServer from './config/http.js';
 
-const runServer = () => {
+const runServer = async () => {
+    await connectDB();
     httpServer.listen(process.env.PORT, () => {
         console.log(`Server running in port ${process.env.PORT}`);
     });

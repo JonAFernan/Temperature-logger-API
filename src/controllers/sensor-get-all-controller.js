dfef3a52dbb1559ac53de607c5b033330d80a524
Jon A.Fernadez <jafernandiaz@gmail.com>
2025-04-26 17:48:47 +0200

    sensor get querys fixed. removed alarm column

--- src/controllers/sensor-get-all-controller.js
+++ src/controllers/sensor-get-all-controller.js
@@ -2,15 +2,16 @@ import pool from '../index.js';
 
 const getAllSensorsController = async (req, res) => {
     const query = `
-        SELECT s.*, r.temperature, r.alarm
-        FROM sensors s
-        LEFT JOIN (
-            SELECT sensor_id, temperature, alarm
-            FROM records r1
-            WHERE record_id = (
-                SELECT MAX(record_id) FROM records r2 WHERE r1.sensor_id = r2.sensor_id
-            )
-        ) r ON s.sensor_id = r.sensor_id;`;
+    SELECT s.*, r.temperature, r.date
+    FROM sensors s
+    LEFT JOIN (
+        SELECT sensor_id, temperature, date
+        FROM records r1
+        WHERE record_id = (
+            SELECT MAX(record_id) FROM records r2 WHERE r1.sensor_id = r2.sensor_id
+        )
+    ) r ON s.sensor_id = r.sensor_id;
+`;
 
     try {
         const [result] = await pool.query(query);
--- src/controllers/sensor-get-by-id-controller.js
+++ src/controllers/sensor-get-by-id-controller.js
@@ -2,17 +2,17 @@ import pool from '../index.js';
 
 const getSensorByIdController = async (req, res) => {
     const query = `
-        SELECT s.*, r.temperature, r.alarm
-        FROM sensors s 
-        LEFT JOIN (
-            SELECT sensor_id, temperature, alarm
-            FROM records r1
-            WHERE record_id = (
-                SELECT MAX(record_id) FROM records r2 WHERE r1.sensor_id = r2.sensor_id
-            )
-        ) r ON s.sensor_id = r.sensor_id
-        WHERE s.sensor_id = ?;
-    `;
+    SELECT s.*, r.temperature, r.date
+    FROM sensors s 
+    LEFT JOIN (
+        SELECT sensor_id, temperature, date
+        FROM records r1
+        WHERE record_id = (
+            SELECT MAX(record_id) FROM records r2 WHERE r1.sensor_id = r2.sensor_id
+        )
+    ) r ON s.sensor_id = r.sensor_id
+    WHERE s.sensor_id = ?;
+`;
 
     const { sensor_id } = req.body;
 

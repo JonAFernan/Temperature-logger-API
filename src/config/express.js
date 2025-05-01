bcd40cf485eabd14e957e983e641ab67bf1ec441
Jon A.Fernadez <jafernandiaz@gmail.com>
2025-05-01 12:15:48 +0200

    Access-Control-Allow-Origin

--- src/config/express.js
+++ src/config/express.js
@@ -6,6 +6,12 @@ import jsonErrorHandler from '../middleware/jsonError.handler.js';
 const expressApp = express();
 
 //Middleware
+expressApp.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 expressApp.use(express.json());
 expressApp.use(jsonErrorHandler);
 //routes
